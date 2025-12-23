33f742e626d7206e3e453b892cce6fb6792dc655
Dustin Myers <MyersDigitalServicesAI@gmail.com>
2025-12-22 23:01:17 -0500

    Add getPricingData function to fetch pricing data
    This function retrieves pricing data from the 1build API based on project type and location.

---
+++ functions/getPricingData.js
@@ -0,0 +1,34 @@
+// functions/getPricingData.js
+const functions = require('firebase-functions');
+const axios = require('axios');
+
+exports.getPricingData = functions.https.onCall(async (data, context) => {
+  const { projectType, county, state } = data;
+  
+  // Call 1build API for real-time pricing
+  const response = await axios.post(
+    'https://api.1build.com/v1/estimate',
+    {
+      projectType: projectType,      // "roofing", "hvac", etc.
+      location: {
+        county: county,
+        state: state
+      },
+      fetchCompetitorData: true       // Get what competitors charge
+    },
+    {
+      headers: {
+        'Authorization': `Bearer ${process.env.ONEBUILD_API_KEY}`,
+        'Content-Type': 'application/json'
+      }
+    }
+  );
+
+  return {
+    materialCosts: response.data.materials,
+    laborCosts: response.data.labor,
+    equipmentCosts: response.data.equipment,
+    competitorRates: response.data.marketComparison,
+    timestamp: new Date()
+  };
+});
